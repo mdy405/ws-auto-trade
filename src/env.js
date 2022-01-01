cda25b116ee18cd9e8aeb237f2b1bd3942317f2c
mdy405 <36553599+mdy405@users.noreply.github.com>
2021-12-31 20:28:15 -0500

    startup

---
+++ src/env.js
@@ -0,0 +1,20 @@
+// read .env file into proces.env
+import dotenv from "dotenv";
+dotenv.config();
+
+import envalid from "envalid";
+
+const configs = envalid.cleanEnv(process.env, {
+  TRADE_USER_POSISTION_ONLY: envalid.bool({ default: true }),
+  WS_EMAIL: envalid.str({ default: "" }),
+  WS_PASS: envalid.str({ default: "" }),
+  USE_GMAIL: envalid.bool({ default: true }),
+  GMAIL_ADDRESS: envalid.str({ default: "" }),
+  SERVER_PORT: envalid.port({
+    default: 4000,
+    desc: "The port to start the server on",
+  }),
+  GMAIL_APP_PASSWORD: envalid.str({ default: "" }),
+});
+
+export default configs;

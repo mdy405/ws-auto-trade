0a64528be75c865e9ecee9102d10a0044947d9db
mdy405 <36553599+mdy405@users.noreply.github.com>
2022-01-01 16:27:15 -0500

    Update env.js

--- src/env.js
+++ src/env.js
@@ -15,6 +15,27 @@ const configs = envalid.cleanEnv(process.env, {
     default: 4000,
     desc: "The port to start the server on",
   }),
+  DATABASE_URL: envalid.str({
+    default:
+      "DATABASE_URL=postgres://wsautotrade:wsautotradeP4$$word@127.0.0.1:5432/wsautotrade",
+  }),
+  DATABASE_URL_SEQUELIZE: envalid.str({
+    default:
+      "postgres://wsautotrade:wsautotradeP4$$word@127.0.0.1:5432/wsautotrade",
+  }),
+  ALPHA_ADVANTAGE_API_KEY: envalid.str({
+    default: "",
+  }),
+  ALPHA_ADVANTAGE_API_URL: envalid.str({
+    default:
+      "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&",
+  }),
+  CRON_SCHEDULE: envalid.str({
+    default: "0 35 9 * * MON-FRI",
+  }),
+  APP_DATE_FORMAT: envalid.str({
+    default: "YYYY-MM-DD",
+  }),
 });
 
 export default configs;

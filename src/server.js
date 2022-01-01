e796faaa618aeb896953bc56540cd0cc17aee522
mdy405 <36553599+mdy405@users.noreply.github.com>
2022-01-01 16:27:19 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import moment from "moment";
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
+import morgan from "morgan";
+import nodeCron from "node-cron";
 /////////////////////////////////////////////////////////////////
 
 //custom import
+import { initDB, updateStock, findAllForRange } from "./utils/stockService.js";
 import env from "./env.js";
 import { loginToWs, getData, getQuote } from "./wshelper/index.js";
 /////////////////////////////////////////////////////////////////
@@ -29,13 +32,48 @@ const exchanges = [
 ];
 /////////////////////////////////////////////////////////////////
 
+const toWatch = ["TSLA:NASDAQ"];
+
+/////////////////////////////////////////////////////////////////
+////////////////////////db///////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+const db = initDB();
+/////////////////////DB end /////////////////////////////////////
+
 //server setup
 const app = express();
+// Log requests to the console.
+app.use(morgan("dev"));
 const server = http.createServer(app);
 const io = new Server(server);
 let wsAuth;
 /////////////////////////////////////////////////////////////////
 let stocks = [];
+let dbUpdated = false;
+
+nodeCron.schedule(env.CRON_SCHEDULE, () => {
+  if (stocks && stocks.length > 0) {
+    let counter = 0;
+    const WAIT_BETWEEN_CALLS = 1000; // 1 second
+    stocks.forEach(function (stock, index) {
+      // This part may get too intensive if tracking lots of stocks.
+      // Please be respectful of Alphavantage's suggestion of max requests per minute
+      if (index <= 3) {
+        setTimeout(function () {
+          console.log("Updating " + stock.split(":")[0]);
+          updateStock(stock.split(":")[0]);
+        }, counter * WAIT_BETWEEN_CALLS);
+        counter++;
+        dbUpdated = true;
+      } else {
+        return;
+      }
+    });
+  } else {
+    console.log("No stocks to update");
+  }
+});
 
 /////////////////////////////////////////////////////////////////
 ////////////////get all ticket available on ws///////////////////
@@ -71,20 +109,35 @@ async function run() {
   let data = getData();
   let quotes = getQuote();
 
-  initStockData(data);
+  //initStockData(data);
+
+  stocks = ["TSLA:NASDAQ"];
 
   if (wsAuth == null || wsAuth == undefined) {
     console.log("login to whealthSimple failed.");
     process.exit(1);
   }
 
+  const dataFrom = moment().subtract(3, "M").format(env.APP_DATE_FORMAT);
+  const dataTo = moment().format(env.APP_DATE_FORMAT);
+
   setInterval(async () => {
-    for (let stock of stocks) {
+    findAllForRange([stocks[0].split(":")[0]], dataFrom, dataTo, (data) => {
+      if (data && data.length > 0) {
+        console.log(data);
+      }
+    });
+    for (let stock of toWatch) {
       console.log("==========>getting quotes for:", stock);
       let currentStockValue = await quotes.get(stock);
       console.log("==========>Current stock value :", currentStockValue);
     }
-  }, 900000);
+
+    /*let stockToWatch = stocks[0];
+    if (stockToWatch) {
+      updateStock(stockToWatch.split(":")[0], db);
+    }*/
+  }, 30000);
 }
 
 //////////////////////start server///////////////////////////////
